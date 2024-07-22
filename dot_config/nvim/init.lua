ba087b6fda820d83f8cf9a419a9a3d2616a35b33
wasabeef <dadadada.chop@gmail.com>
2024-07-22 12:19:55 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -845,9 +845,6 @@ require("lazy").setup({
     -- コードハイライト
     {
       "nvim-treesitter/nvim-treesitter",
-      dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-      },
       build = ":TSUpdate",
       event = "VeryLazy",
       init = function(plugin)
@@ -902,25 +899,6 @@ require("lazy").setup({
               end
             end,
             additional_vim_regex_highlighting = false,
-          },
-          textobjects = {
-            select = {
-              enable = true,
-              lookahead = true,
-              keymaps = {
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-                ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-              },
-              selection_modes = {
-                ["@parameter.outer"] = "v", -- charwise
-                ["@function.outer"] = "V", -- linewise
-                ["@class.outer"] = "<c-v>", -- blockwise
-              },
-              include_surrounding_whitespace = true,
-            },
           },
         })
       end,
