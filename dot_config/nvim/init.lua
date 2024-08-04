06b5afb9de76af066ceffb745c665ab8a101e2bb
wasabeef <dadadada.chop@gmail.com>
2024-08-05 00:00:19 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -800,7 +800,15 @@ require('lazy').setup {
     -- コメントアウト
     {
       'numToStr/Comment.nvim',
+      dependencies = {
+        'JoosepAlviste/nvim-ts-context-commentstring',
+      },
       event = 'VeryLazy',
+      config = function()
+        require('Comment').setup {
+          pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+        }
+      end,
     },
 
     -- 括弧
